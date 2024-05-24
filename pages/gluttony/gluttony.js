22edd709f3f1a2c0ea6a4e021d6e24cd4161dee1
TalilahRobbins <162518164+TalilahRobbins@users.noreply.github.com>
2024-05-24 14:38:40 +1000

    Update gluttony.js

--- pages/gluttony/gluttony.js
+++ pages/gluttony/gluttony.js
@@ -1,119 +1,17 @@
-// Paste your JavaScript code here
 let x = (y = 0);
 const fontSize = 16;
 const noiseScale = 0.0004;
-const noiseStrength = 30; // Strength of noise effect
 const foodEmojis = [
-  "🍏",
-  "🍎",
-  "🍐",
-  "🍊",
-  "🍋",
-  "🍌",
-  "🍉",
-  "🍇",
-  "🍓",
-  "🍈",
-  "🍒",
-  "🍑",
-  "🍍",
-  "🥭",
-  "🍅",
-  "🍆",
-  "🥑",
-  "🥦",
-  "🥒",
-  "🥬",
-  "🌶️",
-  "🌽",
-  "🥕",
-  "🥔",
-  "🍠",
-  "🥐",
-  "🍞",
-  "🥖",
-  "🥨",
-  "🧀",
-  "🥚",
-  "🍳",
-  "🥓",
-  "🥩",
-  "🍗",
-  "🍖",
-  "🦴",
-  "🌭",
-  "🍔",
-  "🍟",
-  "🍕",
-  "🥪",
-  "🥙",
-  "🌮",
-  "🌯",
-  "🥗",
-  "🥘",
-  "🍲",
-  "🥣",
-  "🥫",
-  "🍱",
-  "🍛",
-  "🍣",
-  "🍤",
-  "🍙",
-  "🍚",
-  "🍘",
-  "🍥",
-  "🥮",
-  "🍢",
-  "🍡",
-  "🍧",
-  "🍨",
-  "🍦",
-  "🥧",
-  "🍰",
-  "🎂",
-  "🧁",
-  "🍮",
-  "🍭",
-  "🍬",
-  "🍫",
-  "🍿",
-  "🧂",
-  "🍩",
-  "🍪",
-  "🌰",
-  "🥜",
-  "🍯",
-  "🥛",
-  "🍼",
-  "☕",
-  "🍵",
-  "🍶",
-  "🍾",
-  "🍷",
-  "🍸",
-  "🍹",
-  "🍺",
-  "🍻",
-  "🥂",
-  "🥃",
-  "🥤",
-  "🧃",
-  "🧉",
-  "🧊",
+  "🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🍈", "🍒", "🍑", "🍍", "🥭", "🍅", "🍆", "🥑", "🥦", "🥒", "🥬",
+  "🌶️", "🌽", "🥕", "🥔", "🍠", "🥐", "🍞", "🥖", "🥨", "🧀", "🥚", "🍳", "🥓", "🥩", "🍗", "🍖", "🦴", "🌭", "🍔", "🍟",
+  "🍕", "🥪", "🥙", "🌮", "🌯", "🥗", "🥘", "🍲", "🥣", "🥫", "🍱", "🍛", "🍣", "🍤", "🍙", "🍚", "🍘", "🍥", "🥮", "🍢",
+  "🍡", "🍧", "🍨", "🍦", "🥧", "🍰", "🎂", "🧁", "🍮", "🍭", "🍬", "🍫", "🍿", "🧂", "🍩", "🍪", "🌰", "🥜", "🍯", "🥛",
+  "🍼", "☕", "🍵", "🍶", "🍾", "🍷", "🍸", "🍹", "🍺", "🍻", "🥂", "🥃", "🥤", "🧃", "🧉", "🧊"
 ];
-
-// Play the chewing sound
-function playChewingSound() {
-  const audio = document.getElementById("chewing-audio");
-  audio.play();
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(fontSize);
-
-  // Trigger audio playback when canvas is clicked
-  canvas.addEventListener("click", playChewingSound);
 }
 
 function draw() {
@@ -123,20 +21,8 @@ function draw() {
     for (let j = 0; j < height; j += fontSize) {
       const n = noise((i + x) * noiseScale, (j + y) * noiseScale, t);
       const index = int(map(n, 0, 1, 0, foodEmojis.length));
-      let chr = foodEmojis[index];
-      // Introduce glitch effect
-      if (random(1) < 0.05) {
-        // Adjust probability for glitchiness
-        chr = random(foodEmojis); // Replace with random emoji
-      }
-      push();
-      // Apply random offset for glitchy movement
-      translate(
-        random(-noiseStrength, noiseStrength),
-        random(-noiseStrength, noiseStrength)
-      );
+      const chr = foodEmojis[index];
       text(chr, i, j);
-      pop();
     }
   }
 }
