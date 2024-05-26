64ebe106a2328cd6360f5b5b2554da0465bf3fc6
Katarina Anik <163375240+anikkat@users.noreply.github.com>
2024-05-26 14:49:02 +1000

    Update pride.js

--- pages/pride/pride.js
+++ pages/pride/pride.js
@@ -1,8 +1,7 @@
 let capture;
-let videoSize = 10; // Initial size of the video
-let centerX, centerY; // Center coordinates for the video
+let videoSize = 10;
+let centerX, centerY;
 
-// Custom list of words
 let customWords = [
   "If I were to slip and fall", 
   "Relinquishing the saddle", 
@@ -19,11 +18,8 @@ let customWords = [
   "And despite all the people",
   "It's far quicker to stroll into hell",
   "And the toll is only one soul"
-
-
 ];
 
-// Index to keep track of the current word
 let currentWordIndex = 0;
 
 function setup() {
@@ -33,47 +29,37 @@ function setup() {
   capture.size(windowWidth, windowHeight);
   capture.hide();
   textAlign(CENTER, CENTER);
-  // Calculate the center of the canvas
   centerX = width / 2;
   centerY = height / 2;
 }
 
 function draw() {
-  clear(); // Clear the canvas to make the background transparent
+  clear(); 
 
-  // Draw the inverted video feed, starting small and growing bigger
   push();
   translate(centerX, centerY);
-  scale(-1, 1); // Flip the video feed horizontally
+  scale(-1, 1);
   image(capture, -videoSize / 2, -videoSize / 2, videoSize, videoSize);
   pop();
 
-  // Increase the video size for the next frame
-  videoSize += 1; // You can adjust the speed of growth by changing this value
+  videoSize += 1;
 }
 
 function mousePressed() {
-  // Generate a new word element
   generateWord();
 }
 
-// Function to generate a new word element
 function generateWord() {
-  // Get the current word from the list
   let currentWord = customWords[currentWordIndex];
 
-  // Create a new word element
   let wordElement = createDiv(currentWord);
 
-  // Set CSS styles for the word element
   wordElement.position(random(width), random(height));
   wordElement.addClass('word');
 
-  // Add click event listener to close the word element when clicked
   wordElement.mousePressed(() => {
     wordElement.remove();
   });
 
-  // Increment the index to move to the next word
   currentWordIndex = (currentWordIndex + 1) % customWords.length;
 }
